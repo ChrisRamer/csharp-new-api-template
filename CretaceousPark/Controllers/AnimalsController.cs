614351858e101b254496ab5bb28022c268aaa247
Chris Ramer <chrisramer.97@gmail.com>
2021-06-08 09:58:55 -0700

    Update to 3.6.0.06

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -1,5 +1,6 @@
 using CretaceousPark.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -36,6 +37,24 @@ namespace CretaceousPark.Controllers
 		public ActionResult<Animal> Get(int id)
 		{
 			return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+		}
+
+		// PUT api/animals/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] Animal animal)
+		{
+			animal.AnimalId = id;
+			_db.Entry(animal).State = EntityState.Modified;
+			_db.SaveChanges();
+		}
+
+		// DELETE api/animals/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+			Animal animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+			_db.Animals.Remove(animalToDelete);
+			_db.SaveChanges();
 		}
 	}
 }
